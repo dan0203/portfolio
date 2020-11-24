217b250c611d99ee85df37c6d695f4bb22f34411
Dan Zerbib <danzerbib@gmail.com>
2020-11-24 17:53:43 +0100

    Technical skills section using Owl Carousel and Pie Chart plugins

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    // Image slider settings
+    // Image slider
     $('#slides').superslides({
         animation: 'fade',
         play: 5000,
@@ -13,5 +13,44 @@ $(document).ready(function(){
         loop: true,
         startDelay: 1000,
         showCursor: false
+    });
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
+        }
+    });
+
+    // Pie Chart
+    // Start chart animation when section shows on scroll
+    var skillsTopOffset = $(".skillSection").offset().top;
+    $(window).scroll(function(){
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+            $('.chart').easyPieChart({
+                easing: 'easInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function (from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
     });
 });
