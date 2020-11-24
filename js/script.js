fcd55081deaa644e2c28f2d32e2ca72dd1a43061
Dan Zerbib <danzerbib@gmail.com>
2020-11-24 16:38:19 +0100

    Title section using CSS and Typed.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+    // Image slider settings
     $('#slides').superslides({
         animation: 'fade',
         play: 5000,
         pagination: false
+    });
+
+    // Typed
+    var typed = new Typed(".typed", {
+        strings: ["Développeur Web.", "Programmeur."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
     });
 });
